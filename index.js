1be3d6bca1f5ab70ae078c47c05591e506188c31
Mohamed Ashraf <mohamedashraff.hussein@gmail.com>
2022-09-24 14:27:03 +0200

    Added a new Feature
    Updated the background image and added get tab link feature

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -24,6 +24,13 @@ deleteBtn.addEventListener("dblclick", function () {
     render(myLeads)
 })
 
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ""
@@ -38,3 +45,6 @@ function render(leads) {
     }
     ulEl.innerHTML = listItems
 }
+
+
+
